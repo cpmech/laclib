fd49861d6d374683046c6b643fb8f2535517bd9b
Dorival Pedroso <dorival.pedroso@gmail.com>
2020-12-12 10:08:06 +1000

    Improve test

--- src/sparse/z_test_read_matrix.cpp
+++ src/sparse/z_test_read_matrix.cpp
@@ -5,10 +5,10 @@
 
 TEST_CASE("testing the return value from read_matrix")
 {
-    // SUBCASE("cannot open file")
-    // {
-    //     CHECK_THROWS_WITH(read_matrix("data/small-sparse-matix.mtx", true), "read_matrix: cannot open file");
-    // }
+    SUBCASE("cannot open file")
+    {
+        CHECK_THROWS_WITH(read_matrix("invalid.mtx", true), "read_matrix: cannot open file");
+    }
 
     SUBCASE("small-sparse-matrix")
     {
