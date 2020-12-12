e835adfa4f604611981d0b9013e8802076a228ab
Dorival Pedroso <dorival.pedroso@gmail.com>
2020-12-12 14:25:57 +1000

    Call file close in read_matrix

--- src/sparse/read_matrix.cpp
+++ src/sparse/read_matrix.cpp
@@ -32,22 +32,27 @@ ReadMatrixResults read_matrix(string filename, bool mirrorIfSym)
                 auto info = string_fields(line);
                 if (info.size() != 5)
                 {
+                    myfile.close();
                     throw "info header starting with %%MatrixMarket is incorrect";
                 }
                 if (info[1] != "matrix")
                 {
+                    myfile.close();
                     throw "can only read \"matrix\" MatrixMarket at the moment";
                 }
                 if (info[2] != "coordinate")
                 {
+                    myfile.close();
                     throw "can only read \"coordinate\" MatrixMarket at the moment";
                 }
                 if (info[3] != "real")
                 {
+                    myfile.close();
                     throw "the given MatrixMarket file must have the word \"real\" in the header";
                 }
                 if (info[4] != "general" && info[4] != "symmetric" && info[4] != "unsymmetric")
                 {
+                    myfile.close();
                     throw "this function only works with \"general\", \"symmetric\" and \"unsymmetric\" MatrixMarket files";
                 }
                 if (info[4] == "symmetric")
@@ -67,6 +72,7 @@ ReadMatrixResults read_matrix(string filename, bool mirrorIfSym)
 
             if (r.size() != 3)
             {
+                myfile.close();
                 throw "the number of columns in the line with dimensions must be 3 (m,n,nnz)";
             }
 
@@ -101,6 +107,7 @@ ReadMatrixResults read_matrix(string filename, bool mirrorIfSym)
 
             if (r.size() != 3)
             {
+                myfile.close();
                 throw "the number of columns in the data lines must be 3 (i,j,x)";
             }
 
