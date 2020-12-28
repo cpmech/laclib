574da697e22187f949f092c5cc01fc3f17ee2e11
Dorival Pedroso <dorival.pedroso@gmail.com>
2020-12-28 11:37:41 +1000

    Implement benchmark

--- benchmarks/sparse/main.cpp
+++ benchmarks/sparse/main.cpp
@@ -1,7 +1,10 @@
 #include "mpi.h"
+#include <string>
 #include <iostream>
 #include "../../src/mpiaux/mpiaux.h"
 #include "../../src/check/check.h"
+#include "../../src/util/catch_all.h"
+#include "../../src/util/extract_arguments.h"
 #include "../../src/util/path_tools.h"
 #include "../../src/sparse/read_matrix_for_mumps.h"
 #include "../../src/sparse/solver_mumps.h"
@@ -9,32 +12,43 @@
 using namespace std;
 
 int main(int argc, char **argv)
+try
 {
     MPI_Init(&argc, &argv);
 
-    auto data_path = path_get_current() + "/../../../data";
-    auto mtx_path = data_path + "/sparse-matrix/bfwb62.mtx";
-    auto data = read_matrix_for_mumps(mtx_path);
+    auto name = extract_first_argument(argc, argv, "bfwb62");
+    auto path = path_get_current() + "/../../../data/sparse-matrix/";
+    auto data = read_matrix_for_mumps(path + name + ".mtx");
+
+    cout << "matrix name = " << name << endl;
 
     auto mpi = MpiAux::make_new();
     auto solver = MumpsSolver::make_new(mpi, data.symmetric);
     auto options = MumpsOptions::make_new();
-    auto rhs_is_distributed = false;
     auto verbose = true;
 
     solver->analize_and_factorize(data.trip.get(), options, verbose);
 
-    size_t n = data.trip->n;
+    auto n = data.trip->n;
     auto rhs = vector<double>(n, 1.0);
     auto x = vector<double>(n, 0.0);
+    auto rhs_is_distributed = false;
 
     solver->solve(x, rhs, rhs_is_distributed, verbose);
 
-    if (equal_vectors_tol(x, bfwb62_x_correct, 1e-10, true))
+    if (name == "bfwb62")
     {
-        cout << "\n### OK ###\n\n";
+        if (equal_vectors_tol(x, bfwb62_x_correct, 1e-10, true))
+        {
+            cout << "\n### OK ###\n\n";
+        }
+        else
+        {
+            cout << "\nERROR\n\n";
+        }
     }
 
     MPI_Finalize();
     return 0;
 }
+CATCH_ALL
