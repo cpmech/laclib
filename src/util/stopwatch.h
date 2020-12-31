883479af9cad8f30eb50344db67a4410e7f18fd6
Dorival Pedroso <dorival.pedroso@gmail.com>
2020-12-31 15:12:54 +1000

    Improve stopwatch

--- src/util/stopwatch.h
+++ src/util/stopwatch.h
@@ -2,39 +2,42 @@
 #include <chrono>
 #include <string>
 #include <iostream>
-#include <sstream>
+#include "nanoseconds_to_string.h"
 
 struct Stopwatch
 {
-    std::chrono::steady_clock::time_point initial_time;
+    std::chrono::time_point<std::chrono::high_resolution_clock> initial_time;
 
     inline static Stopwatch make_new()
     {
         return {
-            std::chrono::steady_clock::now(),
+            std::chrono::high_resolution_clock::now(),
         };
     };
 
     // returns the elased time as string
+    // INPUT:
+    //   prefix != "" => will print to the terminal too
+    //   do_reset => will reset initial time to the current time
     inline std::string stop(const std::string &prefix, bool do_reset = false)
     {
-        auto end = std::chrono::steady_clock::now();
-        std::chrono::duration<double> elapsed_seconds = end - this->initial_time;
-        std::ostringstream oss;
-        oss << elapsed_seconds.count() << "s";
+        auto end = std::chrono::high_resolution_clock::now();
+        auto delta = std::chrono::duration_cast<std::chrono::nanoseconds>(end - this->initial_time);
+        int64_t nanoseconds = std::chrono::duration<int64_t, std::nano>(delta).count();
+        auto str = nanoseconds_to_string(nanoseconds);
         if (prefix != "")
         {
-            std::cout << prefix << "elapsed time = " << oss.str() << std::endl;
+            std::cout << prefix << "elapsed time = " << str << std::endl;
         }
         if (do_reset)
         {
             this->reset();
         }
-        return oss.str();
+        return str;
     }
 
     inline void reset()
     {
-        this->initial_time = std::chrono::steady_clock::now();
+        this->initial_time = std::chrono::high_resolution_clock::now();
     }
 };
--- src/util/z_test_stopwatch.cpp
+++ src/util/z_test_stopwatch.cpp
@@ -5,19 +5,22 @@
 
 TEST_CASE("stopwatch")
 {
+    auto sw_total = Stopwatch::make_new();
     auto sw = Stopwatch::make_new();
 
     sleep(333);
 
-    auto str_et = sw.stop("A: ");
-    CHECK(str_et.substr(0, 5) == "0.333");
-    CHECK(str_et.back() == 's');
-
-    sw.reset();
+    auto str_et = sw.stop("A: ", true);
+    CHECK(str_et.substr(0, 4) == "333.");
+    CHECK(str_et.substr(str_et.size() - 2) == "ms");
 
     sleep(444);
 
     str_et = sw.stop("B: ");
-    CHECK(str_et.substr(0, 5) == "0.444");
-    CHECK(str_et.back() == 's');
+    CHECK(str_et.substr(0, 4) == "444.");
+    CHECK(str_et.substr(str_et.size() - 2) == "ms");
+
+    auto str_total_et = sw_total.stop("C: ");
+    CHECK(str_total_et.substr(0, 4) == "777.");
+    CHECK(str_total_et.substr(str_total_et.size() - 2) == "ms");
 }
