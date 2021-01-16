2be1e8a827e714b961c925cf8b14f61473b29052
Dorival Pedroso <dorival.pedroso@gmail.com>
2021-01-16 18:29:59 +1000

    Improve analysis tool

--- benchmarks/sparse/analysis/src/index.ts
+++ benchmarks/sparse/analysis/src/index.ts
@@ -1,9 +1,33 @@
-import fs from 'fs';
+import { genTable } from './genTable';
 import { readReport } from './readReport';
 
 async function run() {
-  // const report = readReport('bfwb62');
-  // console.log(report);
+  const matrices: string[] = [
+    'av41092',
+    'bfwb62',
+    'Flan_1565',
+    'helm2d03',
+    'inline_1',
+    'oilpan',
+    'pre2',
+    'tmt_unsym',
+    'twotone',
+  ];
+
+  const nums: number[] = [1, 2, 3, 4];
+
+  matrices.forEach((mat) => {
+    const mpiReports = nums.map((n) => readReport(mat, n));
+    const ompReports = nums.map((n) => readReport(mat, 1, n));
+    const mpiTable = genTable(mat, mpiReports);
+    const ompTable = genTable(mat, ompReports, true);
+    console.log(`## ${mat} matrix\n`);
+    console.log('### MPI\n');
+    console.log(mpiTable);
+    console.log('### OpenMP\n');
+    console.log(ompTable);
+    console.log('\n\n');
+  });
 }
 
 (async () => {
