7e9126a7b30cc3bc033ac44e898707ee23feca95
Dorival Pedroso <dorival.pedroso@gmail.com>
2021-01-18 19:39:34 +1000

    Hide docker message

--- build-docker-image.bash
+++ build-docker-image.bash
@@ -8,4 +8,4 @@ else
     docker build --no-cache -t laclib_open . -f zscripts/Dockerfile-open
 fi
 
-docker images -q -f "dangling=true" | xargs docker rmi
+docker images -q -f "dangling=true" | xargs docker rmi 2>/dev/null
