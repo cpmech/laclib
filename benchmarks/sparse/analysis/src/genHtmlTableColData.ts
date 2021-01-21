d9c211d2d3f6b54ff27931f1b14eb2ab71700308
Dorival Pedroso <dorival.pedroso@gmail.com>
2021-01-21 19:07:57 +1000

    Show speedup in table

--- benchmarks/sparse/analysis/src/genHtmlTableColData.ts
+++ benchmarks/sparse/analysis/src/genHtmlTableColData.ts
@@ -1,5 +1,16 @@
 import { IHtmlColData, IHtmlStyles, IReportsAndAnalysis } from './types';
 
+const emojiWorried = ':worried:';
+
+// prettyNum returns a text representation of FP numbers in a nice format
+const prettyNum = (x: number, width: number = 5, numDecIfTooLong: number = 3): string => {
+  let txt = String(x);
+  if (txt.length > width) {
+    txt = String(x.toFixed(numDecIfTooLong));
+  }
+  return txt;
+};
+
 // example:
 //   label         columns
 //  "intel_mpi#" : [rpt_for_mpi1, rpt_for_mpi2, rpt_for_mpi3, ...]
@@ -7,26 +18,37 @@ export const genHtmlTableColData = (
   label: string,
   dataset: IReportsAndAnalysis,
   styles?: IHtmlStyles,
+  speedupWorryThreshold = 0.5,
 ): IHtmlColData => {
   // select styles
   const styDat = styles ? styles.tabData : '';
   const styLab = styles ? styles.tabDataLabel : '';
 
   // function to generate link
-  const link = (entry: string, link?: string) => (link ? `<a href="${link}">${entry}</a>` : entry);
+  const ahref = (entry: string, link?: string) => (link ? `<a href="${link}">${entry}</a>` : entry);
 
   // auxiliary
-  const { reports } = dataset;
+  const { reports, analysis } = dataset;
 
   // analyze
-  const analyze = reports.map(
+  const analyzeColumns = reports.map(
     (report) => `<td${styDat}>${report.StepAnalyze.ElapsedTimeString}</td>`,
   );
 
   // factorize columns
-  const factorizeColumns = reports.map(
-    (report) => `<td${styDat}>${link(report.StepFactorize.ElapsedTimeString, report.link)}</td>`,
-  );
+  let factorizeColumns: string[];
+  if (analysis) {
+    factorizeColumns = reports.map((report, j) => {
+      const link = ahref(report.StepFactorize.ElapsedTimeString, report.link);
+      const speedup = analysis.fac.speedup[j];
+      const blip = speedup <= speedupWorryThreshold ? emojiWorried : '';
+      return `<td${styDat}>${link}${blip}<br/>(${prettyNum(speedup)})</td>`;
+    });
+  } else {
+    factorizeColumns = reports.map(
+      (report) => `<td${styDat}>${ahref(report.StepFactorize.ElapsedTimeString, report.link)}</td>`,
+    );
+  }
 
   // solve columns
   const solveColumns = reports.map(
@@ -34,7 +56,7 @@ export const genHtmlTableColData = (
   );
 
   // total columns
-  const total = reports.map((report) => `<td${styDat}>${report.TimeSolverString}</td>`);
+  const totalColumns = reports.map((report) => `<td${styDat}>${report.TimeSolverString}</td>`);
 
   // normInfAx columns
   const normInfAxColumns = reports.map((report) => `<td${styDat}>${report.Stats.NormInfAx}</td>`);
@@ -45,15 +67,20 @@ export const genHtmlTableColData = (
   );
 
   // label column
-  const labelColumn = `<td${styLab}>${label}</td>\n    `;
+  const sep = '\n    ';
+  const labelColumn = `<td${styLab}>${label}</td>${sep}`;
+
+  // label column for Factorize
+  const labelColumnF = analysis
+    ? `<td${styLab}>${label}<br/>(speedup)</td>${sep}`
+    : `<td${styLab}>${label}</td>${sep}`;
 
   // results
-  const sep = '\n    ';
   return {
-    Analyze: labelColumn + analyze.join(sep),
-    Factorize: labelColumn + factorizeColumns.join(sep),
+    Analyze: labelColumn + analyzeColumns.join(sep),
+    Factorize: labelColumnF + factorizeColumns.join(sep),
     Solve: labelColumn + solveColumns.join(sep),
-    Total: labelColumn + total.join(sep),
+    Total: labelColumn + totalColumns.join(sep),
     'Norm(A.x)': labelColumn + normInfAxColumns.join(sep),
     'Rel Error': labelColumn + relErrorColumns.join(sep),
   };
