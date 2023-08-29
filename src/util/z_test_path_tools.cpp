e3775c033caca03baa80c52704eeb53e87fe0080
Dorival Pedroso <dorival.pedroso@gmail.com>
2023-08-29 21:19:02 +1000

    Rename test files

--- src/util/z_test_path_tools.cpp
+++ src/util/z_test_path_tools.cpp
@@ -1,12 +1,13 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 #include "path_tools.h"
+#include <iostream>
 
 TEST_CASE("testing path_tools") {
     SUBCASE("path_get_self_exe") {
         auto self_exe = path_get_self_exe();
         CHECK(self_exe.size() > 17);
-        CHECK(self_exe.substr(self_exe.size() - 13) == "zz_path_tools");
+        CHECK(self_exe.substr(self_exe.size() - 17) == "z_test_path_tools");
     }
 
     SUBCASE("path_get_current") {
