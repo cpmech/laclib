001800b2b5fcae97d7b37c99b0c9ac7ca5b25a08
Dorival Pedroso <dorival.pedroso@gmail.com>
2023-08-30 18:18:18 +1000

    Use C++ 20

--- src/sparse/sparse_triplet.cpp
+++ src/sparse/sparse_triplet.cpp
@@ -2,15 +2,15 @@
 
 #include <iostream>
 #include <memory>
+#include <tuple>
 #include <vector>
-
-#include "dmumps_c.h"
-#include "stdio.h"
 
 void SparseTriplet::put(size_t i_zero_based,
                         size_t j_zero_based,
                         double x,
                         bool check_overflow) {
+
+    // check range
     if (i_zero_based < 0 || i_zero_based >= this->m) {
         throw "SparseTriplet::put: index of row is outside range";
     }
@@ -20,23 +20,37 @@ void SparseTriplet::put(size_t i_zero_based,
     if (this->pos >= this->max) {
         throw "SparseTriplet::put: max number of items has been exceeded";
     }
-    if (this->one_based) {
-        if (check_overflow) {
-            this->I[this->pos] = make_mumps_int(i_zero_based + 1);
-            this->J[this->pos] = make_mumps_int(j_zero_based + 1);
-        } else {
-            this->I[this->pos] = static_cast<MUMPS_INT>(i_zero_based + 1);
-            this->J[this->pos] = static_cast<MUMPS_INT>(j_zero_based + 1);
-        }
+
+    // find existent entry
+    ij_pair_t ij = {i_zero_based, j_zero_based};
+    auto result = this->ij_to_pos.find(ij);
+
+    if (result != this->ij_to_pos.end()) {
+        // sum duplicate entry
+        auto p = result->second; // pos
+        this->X[p] += x;
+
     } else {
-        if (check_overflow) {
-            this->I[this->pos] = make_mumps_int(i_zero_based);
-            this->J[this->pos] = make_mumps_int(j_zero_based);
+        // assign new entry
+        if (this->one_based) {
+            if (check_overflow) {
+                this->I[this->pos] = make_int(i_zero_based + 1);
+                this->J[this->pos] = make_int(j_zero_based + 1);
+            } else {
+                this->I[this->pos] = static_cast<INT>(i_zero_based + 1);
+                this->J[this->pos] = static_cast<INT>(j_zero_based + 1);
+            }
         } else {
-            this->I[this->pos] = static_cast<MUMPS_INT>(i_zero_based);
-            this->J[this->pos] = static_cast<MUMPS_INT>(j_zero_based);
+            if (check_overflow) {
+                this->I[this->pos] = make_int(i_zero_based);
+                this->J[this->pos] = make_int(j_zero_based);
+            } else {
+                this->I[this->pos] = static_cast<INT>(i_zero_based);
+                this->J[this->pos] = static_cast<INT>(j_zero_based);
+            }
         }
+        this->ij_to_pos[ij] = this->pos;
+        this->X[this->pos] = x;
+        this->pos++;
     }
-    this->X[this->pos] = x;
-    this->pos++;
 }
