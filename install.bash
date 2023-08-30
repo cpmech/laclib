09bd0bbcc28b2f54cf96b95bb681d4937cc409a3
Dorival Pedroso <dorival.pedroso@gmail.com>
2023-08-30 18:43:42 +1000

    Fix install script

--- install.bash
+++ install.bash
@@ -2,17 +2,32 @@
 
 set -e
 
+# arguments
+MKL=${1:-"ON"}
+OMP=${2:-"OFF"}
+
 # options
 PREFIX="/usr/local"
 INCDIR=$PREFIX/include/laclib
 LIBDIR=$PREFIX/lib/laclib
 
+# platform suffix
+PLAT=""
+if [ "${MPI}" = "ON" ]; then
+    PLAT="_intel"
+else
+    PLAT="_open_seq"
+    if [ "${OMP}" = "ON" ]; then
+        PLAT="${PLAT}_omp"
+    fi
+fi
+
 # remove previous build
 rm -rf ./build
 
 # call cmake
-cmake -D A0_MKL="ON" \
-      -D A1_OMP="ON" \
+cmake -D A0_MKL=${MKL} \
+      -D A1_OMP=${OMP} \
       -D A2_OPTIMIZED="ON" \
       -D A3_VERBOSE="OFF" \
       -D CMAKE_BUILD_TYPE="Release" \
@@ -39,7 +54,7 @@ done
 # copy main include file and library
 sudo mkdir -p $LIBDIR
 sudo cp -av src/laclib.h $INCDIR/
-sudo cp -av build/liblaclib.so $LIBDIR/
+sudo cp -av build/liblaclib${PLAT}.so $LIBDIR/
 
 # update ldconfig
 echo "${LIBDIR}" | sudo tee /etc/ld.so.conf.d/laclib.conf >/dev/null
