5da80a85e3443d4a85bf198a1fe98f5d3430c354
Dorival Pedroso <dorival.pedroso@gmail.com>
2023-08-30 18:34:36 +1000

    Update bash and docker scripts

--- install-all-open.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-bash ./install.bash OFF
-bash ./install.bash ON
-
--- install.bash
+++ install.bash
@@ -2,25 +2,17 @@
 
 set -e
 
-# arguments
-OMP=${1:-"ON"}
-
 # options
 PREFIX="/usr/local"
 INCDIR=$PREFIX/include/laclib
 LIBDIR=$PREFIX/lib/laclib
 
-# platform suffix
-PLAT="_open_seq"
-if [ "${OMP}" = "ON" ]; then
-    PLAT="${PLAT}_omp"
-fi
-
 # remove previous build
 rm -rf ./build
 
 # call cmake
-cmake -D A1_OMP=${OMP} \
+cmake -D A0_MKL="ON" \
+      -D A1_OMP="ON" \
       -D A2_OPTIMIZED="ON" \
       -D A3_VERBOSE="OFF" \
       -D CMAKE_BUILD_TYPE="Release" \
@@ -28,7 +20,7 @@ cmake -D A1_OMP=${OMP} \
 
 # compile the library
 cd build
-make laclib${PLAT}
+make && make test
 cd ..
 
 # copy internal include files
@@ -39,7 +31,6 @@ SUBDIRS="
     sparse
     util
 "
-
 for dir in $SUBDIRS; do
     sudo mkdir -p $INCDIR/$dir
     sudo cp -av src/$dir/*.h $INCDIR/$dir/
@@ -48,7 +39,7 @@ done
 # copy main include file and library
 sudo mkdir -p $LIBDIR
 sudo cp -av src/laclib.h $INCDIR/
-sudo cp -av build/liblaclib${PLAT}.so $LIBDIR/
+sudo cp -av build/liblaclib.so $LIBDIR/
 
 # update ldconfig
 echo "${LIBDIR}" | sudo tee /etc/ld.so.conf.d/laclib.conf >/dev/null
