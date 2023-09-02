d8c3ca352e811882aa2ea28d851fd46915fe5cd3
Dorival Pedroso <dorival.pedroso@gmail.com>
2023-09-02 15:29:24 +1000

    [wip] Implement benchmark coo to csr

---
+++ benchmarks/coo-to-csr/main.cpp
@@ -0,0 +1,16 @@
+#include <string>
+
+#include "../../src/laclib.h"
+
+using namespace std;
+
+void run(int argc, char **argv) {
+}
+
+int main(int argc, char **argv) {
+    try {
+        run(argc, argv);
+    }
+    CATCH_ALL
+    return 0;
+}
