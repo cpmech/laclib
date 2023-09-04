f5142f801ee79adeae2a85d0a3b90e1c488082f6
Dorival Pedroso <dorival.pedroso@gmail.com>
2023-09-04 11:08:51 +1000

    Add install deps script

---
+++ install-deps.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+# fake sudo function to be used by docker build
+sudo () {
+  [[ $EUID = 0 ]] || set -- command sudo "$@"
+  "$@"
+}
+
+# install packages
+sudo apt-get update -y && \
+sudo apt-get install -y --no-install-recommends \
+    cmake \
+    g++ \
+    gdb \
+    gfortran \
+    git \
+    liblapacke-dev \
+    libmetis-dev \
+    libopenblas-dev \
+    make \
+    patch
+
+bash ./zscripts/install-intel-mkl-linux.bash
