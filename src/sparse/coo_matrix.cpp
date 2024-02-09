10ef87b538a4db08206e0bb8871495c925ae12b6
Dorival Pedroso <dorival.pedroso@gmail.com>
2024-02-09 11:09:02 +1000

    Simplify the code

--- src/sparse/coo_matrix.cpp
+++ src/sparse/coo_matrix.cpp
@@ -155,7 +155,9 @@ int coo_to_csc(
 
     // construct the column form
     for (int i = 0; i < nrow; i++) {
-        for (int p = rp[i]; p < rp[i] + rc[i]; p++) {
+        int p1 = rp[i];
+        int p2 = p1 + rc[i];
+        for (int p = p1; p < p2; p++) {
             int j = rj[p];
             int cp = w[j];
             bi[cp] = i;
